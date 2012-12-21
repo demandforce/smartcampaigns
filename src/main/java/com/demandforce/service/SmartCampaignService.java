5e8d2c8cf0f572e214c568992595e728efbea62a
weifeng.li <wli@demandforce.com>
2012-12-21 14:48:44 -0800

    again

--- src/main/java/com/demandforce/service/SmartCampaignService.java
+++ src/main/java/com/demandforce/service/SmartCampaignService.java
@@ -22,9 +22,13 @@ public class SmartCampaignService {
 			alertBusiness = getAlertBusiness(businessId);
 			alertBusiness.setEmails(getUserEmails(businessId));
 			
+			long templateId = getMostPopularCampaignTemplateId(businessId);
+			String templateName = getMostPopularCampaignTemplateName(templateId);
+			System.out.println("templateId = " + templateId +", templateName = " + templateName);
+			
 			alertBusiness.setSlowWeek(getNextWeek().get(Calendar.WEEK_OF_YEAR));
-			alertBusiness.setActionTitle("10% Off next week");
-			alertBusiness.setActionUrl("https://172.16.12.15/bp2/campaigns/promotion/step/selectMessageProcess.jsp?campaignId=&campaignTemplateId=2587&cat=1");
+			alertBusiness.setActionTitle(templateName);
+			alertBusiness.setActionUrl("https://172.16.12.15/bp2/campaigns/promotion/step/selectMessageProcess.jsp?campaignId=&campaignTemplateId=" + templateId + "&cat=1");
 			alertBusiness.setMessageText("We suggest you create a campaign. Here is one that works best for businesses in your industry.");
 			alertBusiness.setMessageTitle("Scheduled appointments for next week is lower than your past history.");
 		}
@@ -133,6 +137,78 @@ public class SmartCampaignService {
 		return result;
 	}
 
+	private long getMostPopularCampaignTemplateId(String businessId) {
+		String sql = "SELECT s.CustomCampaigntemplateId " +
+                "FROM temp_conversion_stats AS s JOIN Business AS b ON s.Industry = b.Industry WHERE b.Id = ? ORDER BY s.ConversionCount DESC LIMIT 1";
+		long result = 0;
+		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnect();
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, businessId);
+            rs = ps.executeQuery();
+            
+			if (rs.next()) {
+				result = rs.getLong("CustomCampaigntemplateId");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (ps != null) {
+					ps.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
+
+	private String getMostPopularCampaignTemplateName(long templateId) {
+		String sql = "SELECT Name " +
+                "FROM CustomCampaignTemplate WHERE Id = ?";
+		String result = "";
+		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnect();
+			ps = conn.prepareStatement(sql);
+			ps.setLong(1, templateId);
+            rs = ps.executeQuery();
+            
+			if (rs.next()) {
+				result = rs.getString("Name");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (ps != null) {
+					ps.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}	
 	private int getWeekAvgApptCount(String businessId, int weekOfYear) {
 		String sql = "SELECT avg(`Count`) AS ApptCount " +
                 "FROM WeeklyScheduledAppointmentsCount where BusinessID = ? AND WeekOfYear = ?";
